b082498ec5760a60a4110a7d04e6075898834f4d
PineappleGuy <garff247@gmail.com>
2019-08-13 15:40:15 -0700

    Created views/sessions login and signup erbs

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
 
     get '/login' do
-
+        erb :'sessions/login'
     end
 
     get '/signup' do
-
+        erb :'sessions/signup'
     end
 
 end
