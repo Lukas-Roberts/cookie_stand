105891931c295a0a05bb3d54b950666f11559bf0
PineappleGuy <garff247@gmail.com>
2019-08-15 11:06:09 -0700

    Added css for failed login message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,12 @@ class SessionsController < ApplicationController
 
     post '/login' do
         user = User.find_by(username: params(:username))
-        session[:user_id] = user.id
+        if !!user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect :'cookies/index'
+        else
+            erb :'sessions/login'
+        end
     end
 
     get '/signup' do
