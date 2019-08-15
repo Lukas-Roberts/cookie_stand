8cb82c45aaa4cba814bda83c01502afa12a9a170
PineappleGuy <garff247@gmail.com>
2019-08-15 10:33:55 -0700

    Added helper methods and login with unique username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,11 @@ class SessionsController < ApplicationController
         erb :'sessions/login'
     end
 
+    post '/login' do
+        user = User.find_by(username: params(:username))
+        session[:user_id] = user.id
+    end
+
     get '/signup' do
         erb :'sessions/signup'
     end
