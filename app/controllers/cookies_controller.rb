e687a162317dcb59b9a7888379bbdd014c526330
Garth Lilly <garff247@gmail.com>
2019-08-16 00:08:08 -0700

    Stubbed out the rest of the requests in the cookie controller

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -12,23 +12,32 @@ class CookiesController < ApplicationController
     end
 
     post '/cookies' do
-        
+        cookie = Cookie.new(name: params[:cookie_name], type: params[:cookie_type], user_id: current_user.id)
+        cookie.save
+        redirect :'cookies/index'
     end
 
     get '/cookies/:id' do
-
+        @cookie = Cookie.find_by_id(params[:id])
+        erb :'cookies/show'
     end
 
     get '/cookies/:id/edit' do
-
+        @cookie = Cookie.find_by_id(params[:id])
+        erb :'cookies/edit'
     end
 
     patch '/cookies/:id' do
-
+        @cookie = Cookie.find_by_id(params[:id])
+        @cookie.name = params[:]
+        @cookie.type = params[:]
+        @cookie.save
+        redirect :'/cookies'
     end
 
-    delete '/cookies/:id' do
-
+    delete '/cookies/:id/delete' do
+        @cookie = Cookie.find_by_id(params[:id])
+        @cookie.delete
     end
 
 
